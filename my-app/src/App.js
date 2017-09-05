1b83ecd22a33b11c25f9e318ba7f240a0e3b1563
keilsmit <keilsmit@yahoo.com>
2017-09-05 18:54:07 -0400

    revised helper method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -114,7 +114,7 @@ render() {
     ];
     for (let i=0; i<lines.length; i++){
       const [a,b,c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && sqaures[a] === squares[c]){
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
         return squares[a]
       }
     }
